2e472ab111f8ad2dda7a70745d617509db66bcd5
ABauschat <abthebest13@googlemail.com>
2024-11-16 22:25:34 +0100

    16.11.24 22:25

--- lib/RemoteControlState.h
+++ lib/RemoteControlState.h
@@ -6,25 +6,47 @@
 #include "Device.h"
 #include "DisplayUtils.h"
 #include "NFCLogic.h"
+#include <esp_now.h>
+#include <WiFi.h>
 
 namespace NuggetsInc
 {
-
     class RemoteControlState : public AppState
     {
-    public:
-        RemoteControlState();
-        ~RemoteControlState() override;
+        struct struct_message
+        {
+            char messageType[10];
+            char command[20]; // Increased to 20 bytes
+        };
 
-        void onEnter() override;
-        void onExit() override;
-        void update() override;
+    public:
+        RemoteControlState();                // Constructor
+        ~RemoteControlState() override;      // Destructor
+
+        void onEnter() override;             // Called when entering this state
+        void onExit() override;              // Called when exiting this state
+        void update() override;              // Called during update loop
 
     private:
         void handleInput(EventType eventType);
         void displayRemoteControlInterface();
-        DisplayUtils *displayUtils;
-        NFCLogic *nfcLogic;
+        void setupESPNow();
+
+        // Static callbacks required by ESP-NOW
+        static void onDataSentCallback(const uint8_t *mac_addr, esp_now_send_status_t status);
+        static void onDataRecvCallback(const uint8_t *mac_addr, const uint8_t *incomingData, int len);
+
+        // Instance methods to handle callbacks
+        void handleOnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+        void handleOnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len);
+
+        DisplayUtils *displayUtils;           // Display utility
+        NFCLogic *nfcLogic;                   // NFC logic
+
+        uint8_t device2MAC[6];                // MAC address of Device 2
+        bool isPeerAdded;                      // Flag to indicate if Device 2 is added as a peer
+
+        static RemoteControlState *activeInstance;  // Pointer to the active instance
     };
 
 } // namespace NuggetsInc
--- src/RemoteControlState.cpp
+++ src/RemoteControlState.cpp
@@ -3,12 +3,22 @@
 #include "Application.h"
 #include "DisplayUtils.h"
 #include "Config.h"
+#include <esp_now.h>
+#include <WiFi.h>
 
 namespace NuggetsInc
 {
+    // Initialize the static pointer to nullptr
+    RemoteControlState *RemoteControlState::activeInstance = nullptr;
 
     RemoteControlState::RemoteControlState()
-        : nfcLogic(new NFCLogic(PN532_IRQ, PN532_RESET)) {}
+        : nfcLogic(new NFCLogic(PN532_IRQ, PN532_RESET)),
+          isPeerAdded(false)
+    {
+        // Hardcode Device 2's MAC address: D8:13:2A:7F:67:D4
+        uint8_t hardcodedMAC[6] = {0xD8, 0x13, 0x2A, 0x7F, 0x67, 0xD4};
+        memcpy(device2MAC, hardcodedMAC, sizeof(device2MAC));
+    }
 
     RemoteControlState::~RemoteControlState()
     {
@@ -17,12 +27,15 @@ namespace NuggetsInc
 
     void RemoteControlState::onEnter()
     {
+        activeInstance = this;  // Set the static instance pointer
         displayUtils = new DisplayUtils(Device::getInstance().getDisplay());
+        setupESPNow();
         displayRemoteControlInterface();
     }
 
     void RemoteControlState::onExit()
     {
+        activeInstance = nullptr;  // Clear the static instance pointer
     }
 
     void RemoteControlState::update()
@@ -46,34 +59,126 @@ namespace NuggetsInc
 
     void RemoteControlState::handleInput(EventType eventType)
     {
+        if (!isPeerAdded)
+        {
+            displayUtils->addToTerminalDisplay("Peer not connected");
+            return;
+        }
+
+        struct_message outgoingMessage;
+        strcpy(outgoingMessage.messageType, "command");
+
         switch (eventType)
         {
         case EVENT_UP:
-            // Send command to move up
+            strcpy(outgoingMessage.command, "MOVE_UP");
             break;
         case EVENT_DOWN:
-            // Send command to move down
+            strcpy(outgoingMessage.command, "MOVE_DOWN");
             break;
         case EVENT_LEFT:
-            // Send command to move left
+            strcpy(outgoingMessage.command, "MOVE_LEFT");
             break;
         case EVENT_RIGHT:
-            // Send command to move right
+            strcpy(outgoingMessage.command, "MOVE_RIGHT");
             break;
         case EVENT_SELECT:
-            // Perform selection or action
+            strcpy(outgoingMessage.command, "SELECT");
             break;
         default:
-            break;
+            return;
+        }
+
+        // Send the command to Device 2 using its MAC address
+        if (esp_now_send(device2MAC, (uint8_t *)&outgoingMessage, sizeof(outgoingMessage)) != ESP_OK)
+        {
+            displayUtils->addToTerminalDisplay("Failed to send command");
+        }
+    }
+
+    void RemoteControlState::setupESPNow()
+    {
+        WiFi.mode(WIFI_STA);
+        WiFi.disconnect();
+
+        if (esp_now_init() != ESP_OK)
+        {
+            displayUtils->displayMessage("ESP-NOW init failed");
+            return;
+        }
+
+        // Register send callback
+        esp_now_register_send_cb(onDataSentCallback);
+
+        // Register receive callback
+        esp_now_register_recv_cb(onDataRecvCallback);
+
+        // Add Device 2 as a peer
+        esp_now_peer_info_t peerInfo = {};
+        memcpy(peerInfo.peer_addr, device2MAC, sizeof(device2MAC));
+        peerInfo.channel = 0;  // Use the current channel
+        peerInfo.encrypt = false;
+
+        if (esp_now_add_peer(&peerInfo) == ESP_OK)
+        {
+            isPeerAdded = true;
+            displayUtils->addToTerminalDisplay("Peer added successfully");
+        }
+        else
+        {
+            displayUtils->addToTerminalDisplay("Failed to add peer");
+        }
+    }
+
+    // Static send callback
+    void RemoteControlState::onDataSentCallback(const uint8_t *mac_addr, esp_now_send_status_t status)
+    {
+        if (activeInstance)
+        {
+            activeInstance->handleOnDataSent(mac_addr, status);
+        }
+    }
+
+    // Static receive callback
+    void RemoteControlState::onDataRecvCallback(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
+    {
+        if (activeInstance)
+        {
+            activeInstance->handleOnDataRecv(mac_addr, incomingData, len);
+        }
+    }
+
+    void RemoteControlState::handleOnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+    {
+        String result = (status == ESP_NOW_SEND_SUCCESS) ? "Send success" : "Send failed";
+        displayUtils->addToTerminalDisplay(result);
+    }
+
+    void RemoteControlState::handleOnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
+    {
+        // Since the peer is already added, no need to add it again
+        // Just process incoming messages (e.g., responses from Device 2)
+
+        struct_message incomingMessage;
+        if (len >= sizeof(struct_message))
+        {
+            memcpy(&incomingMessage, incomingData, sizeof(incomingMessage));
+
+            if (strcmp(incomingMessage.messageType, "response") == 0)
+            {
+                String response = String(incomingMessage.command);
+                displayUtils->addToTerminalDisplay("Received: " + response);
+            }
+        }
+        else
+        {
+            displayUtils->addToTerminalDisplay("Received invalid message");
         }
     }
 
     void RemoteControlState::displayRemoteControlInterface()
     {
-        // Display remote control interface
-        displayUtils->newTerminalDisplay("Remote Control Interface");
-        displayUtils->addToTerminalDisplay("Use arrow keys to navigate");
-        displayUtils->addToTerminalDisplay("Press SELECT to perform action");
+        displayUtils->newTerminalDisplay("Scan NFC tag to control device");
     }
 
 } // namespace NuggetsInc
