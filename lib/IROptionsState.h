5b8bc18bace5265c63cd406fac37ee899d33feea
ABauschat <abthebest13@googlemail.com>
2024-12-04 01:19:04 +0100

    04.12.24 01:19

--- lib/IROptionsState.h
+++ lib/IROptionsState.h
@@ -2,20 +2,29 @@
 #define IR_OPTIONS_STATE_H
 
 #include "State.h"
+#include "DisplayUtils.h"
 
-namespace NuggetsInc
-{
+namespace NuggetsInc {
 
-    class IROptionsState : public AppState
-    {
-    public:
-        IROptionsState();
-        ~IROptionsState() override;
+class IROptionsState : public AppState {
+public:
+    IROptionsState();
+    ~IROptionsState() override;
 
-        void onEnter() override;
-        void onExit() override;
-        void update() override;
-    };
+    void onEnter() override;
+    void onExit() override;
+    void update() override;
+
+private:
+    void displayMenu();
+    void executeSelection();
+
+    static const int menuItems = 2; // Two options: Remote Browser and Setup New Remote
+    String menu[menuItems];
+    int menuIndex;
+
+    DisplayUtils* displayUtils;
+};
 
 } // namespace NuggetsInc
 
--- src/IROptionsState.cpp
+++ src/IROptionsState.cpp
@@ -1,25 +1,96 @@
 #include "IROptionsState.h"
+#include "Device.h"
+#include "StateFactory.h"
+#include "Application.h"
+#include "EventManager.h"
+#include "Colors.h"
 
-namespace NuggetsInc
-{
+namespace NuggetsInc {
 
-    IROptionsState::IROptionsState() {}
+IROptionsState::IROptionsState()
+    : menuIndex(0),
+      displayUtils(nullptr) {
+    
+    // Define menu options
+    menu[0] = "Remote Browser";
+    menu[1] = "Setup New Remote";
 
-    IROptionsState::~IROptionsState() {}
+    // Initialize DisplayUtils
+    displayUtils = new DisplayUtils(Device::getInstance().getDisplay());
+}
 
-    void IROptionsState::onEnter()
-    {
-        // Placeholder for entering IROptionsState
+IROptionsState::~IROptionsState() {
+    delete displayUtils;
+}
+
+void IROptionsState::onEnter() {
+    displayMenu();
+}
+
+void IROptionsState::onExit() {
+    // Clean up or reset if necessary
+}
+
+void IROptionsState::update() {
+    EventManager& eventManager = EventManager::getInstance();
+    Event event;
+
+    while (eventManager.getNextEvent(event)) {
+        switch (event.type) {
+            case EVENT_UP:
+                menuIndex--;
+                if (menuIndex < 0)
+                    menuIndex = menuItems - 1; // Wrap to last menu item
+                displayMenu();
+                break;
+            case EVENT_DOWN:
+                menuIndex++;
+                if (menuIndex >= menuItems)
+                    menuIndex = 0; // Wrap to first menu item
+                displayMenu();
+                break;
+            case EVENT_ACTION_ONE:
+                executeSelection();
+                break;
+            case EVENT_ACTION_TWO:
+                Application::getInstance().changeState(StateFactory::createState(MENU_STATE));
+                break;
+            default:
+                break;
+        }
     }
+}
 
-    void IROptionsState::onExit()
-    {
-        // Placeholder for exiting IROptionsState
-    }
+void IROptionsState::displayMenu() {
+    Arduino_GFX* gfx = Device::getInstance().getDisplay();
+    gfx->fillScreen(COLOR_BLACK);
+    gfx->setTextSize(2);
 
-    void IROptionsState::update()
-    {
-        // Placeholder for IROptionsState update logic
+    for (int i = 0; i < menuItems; i++) {
+        if (i == menuIndex) {
+            gfx->setTextColor(COLOR_ORANGE); // Highlight selected item
+        } else {
+            gfx->setTextColor(COLOR_WHITE);
+        }
+        gfx->setCursor(10, i * 30);
+        gfx->println(menu[i].c_str());
     }
+}
+
+void IROptionsState::executeSelection() {
+    Application& app = Application::getInstance();
+
+    switch (menuIndex) {
+        case 0: // Remote Browser
+            app.changeState(StateFactory::createState(REMOTE_BROWSER_STATE));
+            break;
+        case 1: // Setup New Remote
+            app.changeState(StateFactory::createState(SETUP_NEW_REMOTE_STATE));
+            break;
+        default:
+            // Handle unexpected cases gracefully
+            break;
+    }
+}
 
 } // namespace NuggetsInc
